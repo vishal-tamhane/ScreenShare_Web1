7505d7f8e5d270f3f7580addab3bd59debcdbe45
vishal-Tamhane <vishaltamhane367@gmail.com>
2025-03-17 22:24:17 +0530

    UI

--- public/js/script.js
+++ public/js/script.js
@@ -15,3 +15,29 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+const vaultDoor = document.querySelector('.vault-door');
+const locks = document.querySelectorAll('.lock');
+
+locks.forEach(lock => {
+  lock.addEventListener('mouseenter', () => {
+    const shadow = lock.getAttribute('data-shadow');
+    vaultDoor.style.setProperty('--vault-shadow', shadow);
+  });
+
+  lock.addEventListener('mouseleave', () => {
+    vaultDoor.style.setProperty('--vault-shadow', 'inset 0 0px 10px rgba(6, 6, 6, 0.5)'); // Reset to default
+  });
+});
+
+const steps = document.querySelectorAll('.step');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('step-visible');
+    }
+  });
+}, { threshold: 0.5 });
+
+steps.forEach(step => observer.observe(step));
